a184e3786c6b120854a90500c5b444a96f945e04
slindsey9150 <stevie.lindsey.jr@gmail.com>
2024-03-04 13:49:22 -0600

    found out how to access the files from phone for upload

--- src/components/EvidenceUpload/EvidenceUpload.jsx
+++ src/components/EvidenceUpload/EvidenceUpload.jsx
@@ -1,16 +1,81 @@
+// * This is the actual rendering of the evidence upload functionality for the user
+// ! The fileinput.onchange has to render after the fileInput has rendered.
+
 export default function EvidenceUpload(props) {
-    return (
-        <div>
-        <>This is where you upload Evidence</>
-        <br/>
-        <form>
-            <label>Input Name</label>
-        <input/>
-        <br/>
-        <input type ="file" id = "fileInput" multiple/>
-        <br/>
-        <textarea placeholder="Optional Notes"/>
-        </form>
-        </div>
-    )
+
+    if (props.type == undefined) {
+        return (
+            <p>No type has been chosen</p>
+        )
+    }
+    else if (props.type == "imageOrVideo") {
+        const fileInput = document.getElementById("fileInput")
+// ? selectedFiles is established outside of the onchange function, so that the handleSubmit function
+// ? can also access it
+        let selectedFiles = ""
+// TODO The onchange function has to render after the fileInput renders to avoid errors.
+// TODO Currently to make the image upload work correctly, the image/video button has to be clicked
+// TODO With the onchange commented out, and then the onchange can be uncommented.
+        fileInput.onchange = () => {
+             selectedFiles = [...fileInput.files];
+            console.log(selectedFiles);
+        }
+        const handleSubmit = () => {
+            console.log("submitting evidence");
+            console.log(selectedFiles);
+        }
+        return (
+            <div>
+                <>This is where you upload images or videos</>
+                <br />
+                <> This is the evidence type: {props.type}</>
+                <br />
+                <form>
+                    <label>Image/Video Name</label>
+                    <input />
+                    <br />
+                    <input type="file" id="fileInput" multiple />
+                    <br />
+                    <button onClick={handleSubmit}>Upload Evidence</button>
+                    <br/>
+                    <textarea placeholder="Optional Notes" />
+                </form>
+            </div>
+        )
+    }
+    else if (props.type == "Text") {
+        return (
+            <div>
+                <>This is where you upload images or videos</>
+                <br />
+                <> This is the evidence type: {props.type}</>
+                <br />
+                <form>
+                    <label>Text Name</label>
+                    <input />
+                    <br />
+                    
+                    <br />
+                    <textarea placeholder="Optional Notes" />
+                </form>
+            </div>
+        )
+    }
+    else if (props.type == "Audio") {
+        return (
+            <div>
+                <>This is where you upload images or videos</>
+                <br />
+                <> This is the evidence type: {props.type}</>
+                <br />
+                <form>
+                    <label>Audio Name</label>
+                    <input />
+                    <br />
+                    <br />
+                    <textarea placeholder="Optional Notes" />
+                </form>
+            </div>
+        )
+    }
 }
