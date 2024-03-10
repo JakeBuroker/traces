9bf33150801219e7ecb57973d4b88ee26317b1eb
JakeBuroker <shamrecks@gmail.com>
2024-03-10 16:42:58 -0500

    added stevie's audio element and gavin's register page formating

---
+++ src/components/Data_practices/tracks.jsx
@@ -0,0 +1,11 @@
+
+
+
+export const tracks = [
+  {
+    title: 'some practice audio',
+    // src: practiceaudio,
+    author: 'me probably',
+    // thumbnail: coverPhoto,
+  }
+];
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { Button } from '@mui/material';
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -29,73 +29,87 @@ function RegisterForm() {
   }; // end registerUser
 
   return (
+    <div className="login-container">
     <form className="formPanel" onSubmit={registerUser}>
-      <h2>Register User</h2>
+      <h2 style={{marginBottom: '50px'}}>Register User</h2>
       {errors.registrationMessage && (
         <h3 className="alert" role="alert">
           {errors.registrationMessage}
         </h3>
       )}
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <label htmlFor="fullName">
-          Full Name:
-        </label>
-        <input
-          type="text"
-          id="fullName"
-          value={fullName}
-          required
-          onChange={(event) => setFullName(event.target.value)}
-        />
-        <label htmlFor="username">
-          Username:
-        </label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          required
-          onChange={(event) => setUsername(event.target.value)}
-        />
-         <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            id="password"
-            value={password}
-            required
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-        <label htmlFor="email">
-          Email:
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div className="input-container">
+          <label htmlFor="fullName">
+            Full Name:
+          </label>
           <input
             type="text"
-            id="email"
-            value={email}
+            id="fullName"
+            value={fullName}
             required
-            onChange={(event) => setEmail(event.target.value)}
+            onChange={(event) => setFullName(event.target.value)}
           />
-        </label>
-        <label htmlFor="phoneNumber">
-          Phone Number:
+        </div>
+        <div className="input-container">
+          <label htmlFor="username">
+            Username:
+          </label>
           <input
             type="text"
-            id="phoneNumber"
-            value={phoneNumber}
+            id="username"
+            value={username}
             required
-            onChange={(event) => setPhoneNumber(event.target.value)}
+            onChange={(event) => setUsername(event.target.value)}
           />
-        </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              id="password"
+              value={password}
+              required
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="email">
+            Email:
+            <input
+              type="text"
+              id="email"
+              value={email}
+              required
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="phoneNumber">
+            Phone Number:
+            <input
+              type="text"
+              id="phoneNumber"
+              value={phoneNumber}
+              required
+              onChange={(event) => setPhoneNumber(event.target.value)}
+            />
+          </label>
+        </div>
         {/* // Todo add a input for role. */}
       </div>
       <div>
-       
+
       </div>
       <div>
-        <input className="btn" type="submit" name="submit" value="Register" />
+        <Button className='btn' type='submit' name='submit' value='Register'>Register</Button>
+        {/* <input className="btn" type="submit" name="submit" value="Register" /> */}
       </div>
     </form>
+  </div>
+
   );
 }
 
