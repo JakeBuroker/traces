e24303eceb3b2e46b136a018c2f60f37bbd20a7b
Gavin Popken <gavinpopkenart@gmail.com>
2024-03-10 15:35:36 -0500

    added a plugin so that the host is served over HTTPS

--- src/components/EvidenceUpload/AudioUploadElement.jsx
+++ src/components/EvidenceUpload/AudioUploadElement.jsx
@@ -24,6 +24,11 @@ export default function AudioUploadElement() {
   } = useAudioRecorder();
 
   useEffect(() => {
+    if (navigator.mediaDevices) {
+      console.log('it is supported');
+    } else {
+      console.log('it isn\'t supported');
+    }
     if (!recordingBlob) return;
 
     // recordingBlob will be present at this point after 'stopRecording' has been called
@@ -44,7 +49,7 @@ export default function AudioUploadElement() {
   return(
     <div>
 
-      <AudioRecorder onClick={console.log('clicking audio record')}
+      <AudioRecorder
          onRecordingComplete={(blob) => addAudioElement(blob)}
          recorderControls={recorderControls}
         audioTrackConstraints={{
@@ -57,8 +62,8 @@ export default function AudioUploadElement() {
           // sampleRate,
           // sampleSize,
         }}
-        onNotAllowedOrFound={(err) => console.table(err)}
-        downloadOnSavePress={true}
+        onNotAllowedOrFound={(err) => {console.log('Not Allowed or Not Found');console.table(err)}}
+        // downloadOnSavePress={true}
         downloadFileExtension="webm"
         mediaRecorderOptions={{
           audioBitsPerSecond: 128000,
@@ -66,6 +71,9 @@ export default function AudioUploadElement() {
         // showVisualizer={true}
       />
       <br />
+      <div className="audioRecorder">
+        <input type="file" accept='audio/*;capture=microphone'/>
+      </div>
     </div>
   );
 }
