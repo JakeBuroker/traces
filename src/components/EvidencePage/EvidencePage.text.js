191d50ca93b4b0f7c30a2975402dc0075931b5c0
Gavin Popken <gavinpopkenart@gmail.com>
2024-09-07 15:23:47 -0500

    styled so the text looks better on desktop

--- src/components/EvidencePage/EvidencePage.text.js
+++ src/components/EvidencePage/EvidencePage.text.js
@@ -1,3 +1,4 @@
 export const evidencePageText = {
-    instructions: "Welcome to TRACES. Tap the red 'Folder' below and select the 'Photo' icon to upload your first photo. You can also upload audio evidence or simple notes. Once you have uploaded a piece of evidence, it will be available for a private investigator to review."
+    welcome: "Welcome to TRACES",
+    instructions: "Tap the red 'Folder' below and select the 'Photo' icon to upload your first photo. You can also upload audio evidence or simple notes. Once you have uploaded a piece of evidence, it will be available for a private investigator to review."
 }
