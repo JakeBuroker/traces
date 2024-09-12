fa58d6dc94f56843bac793d20bc37f5159b8f3db
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-09-11 21:43:37 -0500

    Added the info for the landing page

--- src/components/LandingPage/LandingPage.text.js
+++ src/components/LandingPage/LandingPage.text.js
@@ -4,11 +4,16 @@ export const text = {
 
         },
         landingPageBodies: {
-            body1: `The premier private investigator company where mysteries unravel and secrets are uncovered.
-            With our cutting-edge technology, keen intuition, and unwavering dedication to justice, Traces is renowned for solving the unsolvable.
-            Whether it's tracking down elusive suspects, delving into clandestine affairs, or unearthing hidden truths,
-            trust Traces to crack the case and deliver justice served with a twist. Join us on this thrilling journey into the heart of the unknown,
-            where every clue is a piece of the puzzle waiting to be solved.`,
+            body1: `Thank you for agreeing to be a civilian informant for TRACES. 
+
+You should have received an email from the “Walker Art Center” confirming the date and time of your investigation work. You also will receive reminders via text message in the days leading up to it. 
+
+On the date and time of your appointment, please stand at the Northeast corner of Hennepin Avenue and South 4th Street in downtown Minneapolis (same block as Minneapolis Central Library) and await a phone call from our agency. We will call you on the number that you shared upon registration. If weather is inclement, please feel free to wait in the entryway of Minneapolis Central Library, just through the first set of glass doors.
+
+Traffic and parking can be difficult downtown, so please be sure to arrive early to get in place. You may use the restroom on the ground level of Minneapolis Central Library if needed prior to your appointment. Please review the training video (available for review under the HELP tab) before arrival. Be sure to fully charge your smartphone and bring wired headphones. 
+
+Click the box with three lines in the upper right corner of this page to navigate this portal and upload any evidence that you collect.
+`,
             },
     }
 }
